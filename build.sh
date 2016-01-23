d7bde62807ddde172c07302daa25b63c6237411d
Julien-Charles Lévesque <levesque.jc@gmail.com>
2016-01-23 10:46:02 -0500

    build mods

--- build.sh
+++ build.sh
@@ -14,22 +14,22 @@
 # (it'll almost certainly be under /usr)
 
 # CUDA toolkit installation directory.
-export CUDA_INSTALL_PATH=$CUDA_HOME #/usr/local/cuda
+export CUDA_INSTALL_PATH=$CUDA_HOME 
 
 # CUDA SDK installation directory.
-export CUDA_SDK_PATH=./cuda_samples/inc #/home/spoon/NVIDIA_GPU_Computing_SDK
+export CUDA_SDK_PATH=./cuda_samples/inc 
 
 # Python include directory. This should contain the file Python.h, among others.
-export PYTHON_INCLUDE_PATH=/software6/apps/python/2.7.5/include/python2.7
-#/usr/include/python2.6
-#~/usr/ve_helios_p3.3.2/include/python3.3m #/usr/include/python2.7
+export PYTHON_INCLUDE_PATH=/usr/include/python2.7
+#/software6/apps/python/2.7.5/include/python2.7
 
 # Numpy include directory. This should contain the file arrayobject.h, among others.
-export NUMPY_INCLUDE_PATH=/software6/apps/python/2.7.5/lib/python2.7/site-packages/numpy/core/include/numpy
-#/home/jclev7/usr/ve_helios_p3.3.2/lib/python3.3/site-packages/numpy/core/include/numpy #/usr/lib/pymodules/python2.7/numpy/core/include/numpy
+export NUMPY_INCLUDE_PATH=/usr/lib/python2.7/dist-packages/numpy/core/include/numpy
+#/software6/apps/python/2.7.5/lib/python2.7/site-packages/numpy/core/include/numpy
 
 # ATLAS library directory. This should contain the file libcblas.so, among others.
-export ATLAS_LIB_PATH=./yum/usr/lib64/atlas #/usr/lib/atlas-base
+export ATLAS_LIB_PATH=/usr/lib/atlas-base
+#./yum/usr/lib64/atlas 
 
 make $*
 
